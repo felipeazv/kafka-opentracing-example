bd9ebf61368126a1ae81b769d2c7a488fb81a237
Felipe Azevedo Sant Anna <Felipe.Azevedo@vanderlande.com>
2020-07-04 14:55:09 +0200

    removed comments

--- trace-ping-pong/src/main/java/com/feazesa/infrastructure/kafka/Kafka.java
+++ trace-ping-pong/src/main/java/com/feazesa/infrastructure/kafka/Kafka.java
@@ -18,7 +18,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
